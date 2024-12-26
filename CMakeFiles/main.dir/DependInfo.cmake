cfb5681a31c8240b72a82f82ac7cd96cd9dcfe42
nagraver <ttolla25@gmail.com>
2024-12-27 02:51:57 +0300

    CMake

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nagraver/Cockroach/Course/files.cpp" "CMakeFiles/main.dir/files.cpp.o" "gcc" "CMakeFiles/main.dir/files.cpp.o.d"
+  "/Users/nagraver/Cockroach/Course/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/nagraver/Cockroach/Course/refactoring.cpp" "CMakeFiles/main.dir/refactoring.cpp.o" "gcc" "CMakeFiles/main.dir/refactoring.cpp.o.d"
+  "/Users/nagraver/Cockroach/Course/scrolling.cpp" "CMakeFiles/main.dir/scrolling.cpp.o" "gcc" "CMakeFiles/main.dir/scrolling.cpp.o.d"
+  "/Users/nagraver/Cockroach/Course/service.cpp" "CMakeFiles/main.dir/service.cpp.o" "gcc" "CMakeFiles/main.dir/service.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/files.cpp.o"
+  "CMakeFiles/main.dir/files.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/refactoring.cpp.o"
+  "CMakeFiles/main.dir/refactoring.cpp.o.d"
+  "CMakeFiles/main.dir/scrolling.cpp.o"
+  "CMakeFiles/main.dir/scrolling.cpp.o.d"
+  "CMakeFiles/main.dir/service.cpp.o"
+  "CMakeFiles/main.dir/service.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
